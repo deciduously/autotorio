c1cd2630308769e0bd6074d98335394fc2b74927
deciduously <github@deciduously.com>
2018-08-06 08:09:28 -0400

    CopperOre, CopperPlate

--- src/item.rs
+++ src/item.rs
@@ -4,6 +4,8 @@ use factory::Rate;
 
 #[derive(Debug, PartialEq)]
 pub enum Item {
+    CopperOre,
+    CopperPlate,
     IronOre,
     IronPlate,
 }
@@ -13,6 +15,11 @@ impl Item {
     pub fn recipe(&self, rate: Rate) -> (Vec<(Item, Rate)>, Vec<(Building, u32)>) {
         use self::Item::*;
         match self {
+            CopperOre => (Vec::new(), Vec::new()),
+            CopperPlate => (
+                vec![(CopperPlate, rate)],
+                vec![(Building::Drill, rate), (Building::Furnace, rate)],
+            ),
             IronOre => (Vec::new(), Vec::new()),
             IronPlate => (
                 vec![(IronOre, rate)],
